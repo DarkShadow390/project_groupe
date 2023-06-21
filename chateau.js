9a9cbbe6d4d22e5271c5bc06f6cb8d830c902650
DarkShadow390 <angeokou39@gmail.com>
2023-06-21 16:39:09 +0200

    page chateau

---
+++ chateau.js
@@ -0,0 +1,26 @@
+let imgCrousel1 = document.getElementById("image1");
+let imgCrousel2 = document.getElementById("image2");
+let imgCrousel3 = document.getElementById("image3");
+
+let discCrousel1 = document.getElementById("disc1");
+let discCrousel2 = document.getElementById("disc2");
+let discCrousel3= document.getElementById("disc3");
+
+
+
+discCrousel1.addEventListener("click", function(){
+    imgCrousel1.style.display = "block";
+    imgCrousel2.style.display = "none";
+    imgCrousel3.style.display = "none";
+
+});
+discCrousel2.addEventListener("click", function(){
+    imgCrousel2.style.display = "block";
+    imgCrousel1.style.display = "none";
+    imgCrousel3.style.display = "none";
+});
+discCrousel3.addEventListener("click", function(){
+    imgCrousel3.style.display = "block";
+    imgCrousel1.style.display = "none";
+    imgCrousel2.style.display = "none";
+});
